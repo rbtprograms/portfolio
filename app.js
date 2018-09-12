a4c06b2b2acc0b3e410d228eaca3a8119d1764e1
rbtprograms <rbt61690@gmail.com>
2018-09-12 09:26:56 -0700

    submitting

---
+++ app.js
@@ -0,0 +1,35 @@
+function loadProjects() {
+  let el = document.getElementById("content");
+  el.innerHTML=
+  `Mount Dessuht Bah Hahbah leaf peepahs I'm tellin' you. Have a good one. front dooryahd clammin' Bangah chimbly yessah-bub, some cunnin can't get theyah from heeyah anuthah nummah than a faht Laum Ipsum smokie door-yahd Katahdin podunk naw, sumpin' fierce cunnin' Mount Dessuht wee bit nippy huntin' deeah chimbly over t', The County Have a good one. wreckah cah what a cahd N'Hampshah Powrtland Museum of Aht back woods alkie some wicked, The County lobstah nummah than a faht stove-up. Cubboard clammin' bogan tunk pig fat up t' camp nummah junkah, cunnin' dingy You is sum wicked suhmart, from away Ahcadiuh down cellah stove-up cubboard Up in thah county nummah than a faht got in a gaum.
+
+  Crunchah leaf peepahs wickid decent Laum Ipsum scrod down east some cunnin, Mount Dessuht Moosetown junkah buggin' mummah what a cahd Hammah Gohd Dammah, feeder' the beans heatah idear slower than molasses going uphill in January, Moody's bub door-yahd huck flatlandas blizzahd hum-dingah, hum-dingah alkie from away Bangah 'Roostik If you can't stand the wintah you don't deserve the summah anuthah nummah.
+  
+  Out in th' willie-wacks wintah bean suppah Auguster tunk way up north railed 'em potatoes yow uns lobstahrin'. Stove up smokie Loyston-Ahban, If you can't stand the wintah you don't deserve the summah puckahbrush hum-dingah blizzahd owt mugup dinnahbucket smokie, naw puckahbrush unthaw Mahdin's Outta Staydahs huck whawf.
+  
+  Tunk mugup dinnahbucket over t' Jeesum Crow grindah wicked pissah muckle riyht on'ta her. Loyston-Ahban paypuh bowee Have a good one. If you can't stand the wintah you don't deserve the summah wicked cunnin' wintah, ayuhpawt mistah man wintah You is sum wicked suhmart Bangah Bah Hahbah crunchah slower than molasses going uphill in January yow uns wee bit nippy. The 'Gash Jeesum Crow swampdonkey I'm tellin' you junkah.
+  
+  Yow uns Fryeburg Fayah sawr crunchah, Katahdin Hammah Gohd Dammah, feeder' the beans railed 'em gettin' ugly can't get theyah from heeyah. Dingy yessah-bub Bangoah. Katahdin fellers junkah wee bit nippy tube steak the pit well theyah ayuhpawt over t' wicked pissah, ayuhpawt scrod whawf the pit I'm tellin' you. Paypuh bowee If you can't stand the wintah you don't deserve the summah paypuh bowee up t' camp stove-up Loyston-Ahban way up north up t' potatoes clam chowdah, nummah than a faht mugup slower than molasses going uphill in January mistah man, down east bean suppah numb crunchah, clammin' Hold'er Newt she's, heddin for da ruhbarb! gettin' ugly nummah than a faht numb up t' Up in thah county Bean's buggin' Moosetown.`;
+};
+
+function loadAbout() {
+  let el = document.getElementById("content");
+  el.innerHTML=
+  `hello<br><br><br>
+  I am Robert but everyone calls me Bobby.<br>
+  So you can call me Robert.<br>
+  I am a digital designer from Portland.<br>
+  Basically it means I can design things and program them.<br>
+  I work a lot with React(+Redux), Express, MongoDB, and Node, but I can do a lot of other stuff too.<br>
+  I would greatly enjoy the opportunity to work with you.<br>
+  But only if you pay me, I dont do free shit unless you are a non-profit, in which case I can do some free shit for you.<br>
+  You can find my contact info in the big contact button in the upper right hand corner.
+  `;
+};
+
+function loadContact() {
+  let el = document.getElementById("content");
+  el.innerHTML=
+  `rbt61690 AT gmail DOT com<br>
+  hit me up on LinkedIn or Github`;
+}
